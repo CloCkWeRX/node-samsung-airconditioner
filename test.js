bf3522f70e256779d3f710a2451fd98c349dcaf7
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-01-19 19:09:22 +1030

    jshint

--- test.js
+++ test.js
@@ -4,15 +4,15 @@ var connecting_to = {};
 
 var known_tokens = {};
 
-
-
-
-
 new API().on('discover', function(aircon) {
 
   if (connecting_to[aircon.options.ip]) { 
     return; 
   }
+  if (aircon.options.ip != '192.168.1.15') {
+    return;
+  }
+  known_tokens[aircon.options.ip] = '98854465-6273-M559-N887-373832354144';
 
   // Do we need to find get a token?
   if (!known_tokens[aircon.options.ip]) {
@@ -35,8 +35,15 @@ new API().on('discover', function(aircon) {
   connecting_to[aircon.options.ip] = true;
   aircon.login(known_tokens[aircon.options.ip], function() {
     aircon.onoff(true);
+  
 
-    setTimeout(function() { aircon.onoff(false); }, 10 * 1000);
+    setTimeout(function() { aircon.onoff(false); }, 5*60 * 1000);
+  }).on('stateChange', function(state) {
+    console.log("State changed");
+    console.log(state);
+  }).on('loginSuccess', function () {
+    console.log("HIDSFDs");
+    aircon.status();
   });
 
 
